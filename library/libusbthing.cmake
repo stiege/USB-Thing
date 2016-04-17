f548ddfb3a228a3d4705859046abd69999007bf9
Alex <alex.hodge@gmail.com>
2016-04-17 22:44:09 +1200

    Add pkg-config for finding libusb

--- library/libusbthing.cmake
+++ library/libusbthing.cmake
@@ -9,8 +9,15 @@ set(USBTHING_SOURCES
 #Add required inclusions
 include_directories(${CMAKE_CURRENT_LIST_DIR}/include)
 
+find_package(PkgConfig)
+pkg_check_modules(LIBUSB libusb-1.0)
+
+link_directories(${LIBUSB_LIBRARY_DIRS})
+include_directories(${LIBUSB_INCLUDE_DIRS}/..)
+
 #Create usbthing driver library
 add_library(usbthing-driver ${USBTHING_SOURCES})
+target_link_libraries(usbthing-driver ${LIBUSB_LIBRARIES})
 
 #Add to convenience variable
 set(LIBS ${LIBS} usbthing-driver usb-1.0)
